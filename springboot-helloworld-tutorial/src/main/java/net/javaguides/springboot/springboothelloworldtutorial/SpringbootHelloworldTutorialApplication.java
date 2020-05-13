e9c117596bef98e5678252e7c5fd55389b220187
April Copes <antdevelopment1@gmail.com>
2020-05-13 16:49:29 -0400

    Starts new example troubleshoots route

---
+++ springboot-helloworld-tutorial/src/main/java/net/javaguides/springboot/springboothelloworldtutorial/SpringbootHelloworldTutorialApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.springboothelloworldtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldTutorialApplication.class, args);
+	}
+
+}
---
+++ springboot-helloworld-tutorial/src/main/java/net/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package net.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
