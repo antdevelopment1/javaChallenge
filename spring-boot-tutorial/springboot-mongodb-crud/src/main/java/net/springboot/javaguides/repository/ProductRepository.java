d7af0ecff515195d30c0c7007aaa4d9493a21c64
April Copes <antdevelopment1@gmail.com>
2020-05-13 18:01:37 -0400

    removes cached files

--- spring-boot-tutorial/springboot-mongodb-crud/src/main/java/net/springboot/javaguides/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package net.springboot.javaguides.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import net.springboot.javaguides.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, Long>{
-
-}
